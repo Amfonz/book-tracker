b01d1eb5f0d8b2f55013535c18aa108b0e84fbc6
Michael Friend <a_michael9@live.com>
2019-12-15 15:46:11 -0500

    Add drop down form outline for book creation

--- books.js
+++ books.js
@@ -18,11 +18,12 @@
 
 let library = [];
 
-function Book(title,author,pages,read){
+function Book(title,author,pages,read,coverURL){
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.cover = coverURL;
 }
 Book.prototype.info = ()=>{
   let info = `${title} by ${author}, ${pages} pages,`;
@@ -30,4 +31,77 @@ Book.prototype.info = ()=>{
     return info + ' read';
   }
   return info + ' not read yet';
+}
+
+function displayNewBook(book) {
+  let bookContainer = document.createElement('div');
+  bookContainer.classList.add('book-container');
+
+  let flipContainer = document.createElement('div');
+  flipContainer.classList.add('flip-inner');
+
+  bookContainer.appendChild(flipContainer);
+
+  let front = document.createElement('div');
+  front.classList.add('front');
+  
+  flipContainer.appendChild(front);
+
+  let cover = document.createElement('img');
+  cover.src = book.cover;
+
+  front.appendChild(cover);
+  
+  let whiteBar = document.createElement('white-bar');
+  let whiteSpan1 = document.createElement('span');
+  whiteSpan1.textContent = "Your";
+  let whiteSpan2 = document.createElement('span');
+  whiteSpan1.textContent = "Library";
+  let whiteBarImage = document.createElement('img');
+  whiteBarImage.src = "https://img.icons8.com/ios-filled/50/000000/open-book.png";
+  
+  whiteBar.appendChild(whiteSpan1);
+  whiteBar.appendChild(whiteSpan2);
+  whiteBar.appendChild(whiteBarImage);
+  front.appendChild(whiteBar);
+
+  let blackBox = document.createElement('div');
+  blackBox.classList.add('black-box') ;
+  let authorFront = document.createElement('p');
+  authorFront.textContent = book.author;
+  let titleFront = document.createElement('p');
+  titleFront.textContent = book.title; 
+
+  blackBox.appendChild(authorFront);
+  blackBox.appendChild(titleFront);
+  front.appendChild(blackBox);
+
+  let back = document.createElement('div');
+  back.classList.add('back');
+
+  flipContainer.appendChild(back);
+
+  let backTitle = document.createElement('p');
+  backTitle.textContent = `Title: ${book.title}`;
+  let backAuthor = document.createElement('p');
+  backAuthor.textContent = `Author: ${book.author}`;
+  let backPages = document.createElement('p');
+  backPages.textContent = `Title: ${book.pages}`;
+  let update = document.createElement('button');
+  update.value = 'Update';
+  let remove = document.createElement('button');
+  remove.value = 'Remove';
+  let read = document.createElement('button');
+  read.value = 'read';
+
+  back.appendChild(backTitle);
+  back.appendChild(backAuthor);
+  back.appendChild(backPages);
+  back.appendChild(update);
+  back.appendChild(remove);
+  back.appendChild(read);
+
+
+  let shelf = document.querySelector('#shelf');
+  shelf.appendChild(bookContainer);
 }
